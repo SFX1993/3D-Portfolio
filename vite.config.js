bdee0b2f5704e3f4126fbcfd322d20caf36a73ef
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-04-23 11:58:45 -0500

    working on the header

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import React from "@vitejs/plugin-react";
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [React(), tailwindcss()],
 });
