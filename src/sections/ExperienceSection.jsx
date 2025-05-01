3411949d394c3e3441855e7dfbfb7a1446290fe0
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-04-30 21:38:36 -0500

    working on the work exp part of the page

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,5 +1,6 @@
+import GlowCard from "../components/GlowCard";
 import TitleHeader from "../components/TitleHeader";
-
+import { expCards } from "../constants/index";
 const ExperienceSection = () => {
   return (
     <section
@@ -7,7 +8,25 @@ const ExperienceSection = () => {
       className="w-full md:mt-40 mt-20 section-padding xl:px-0"
     >
       <div className="w-full h-full md:px-20 px-5">
-        <TitleHeader />
+        <TitleHeader
+          title="Professional Work Experience"
+          sub="My Career Overview"
+        />
+        <div className="mt-32 relative">
+          <div className="relative z-50 xl:space-y-32 space-y-10">
+            {expCards.map((card, index) => (
+              <div key={card.title} className="exp-card-wrapper">
+                <div className="xl:w-2/6">
+                  <GlowCard card={card} index={index}>
+                    <div>
+                      <img src={card.imgPath} alt={card.title} />
+                    </div>
+                  </GlowCard>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
