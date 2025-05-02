b819cc2d2a7b4c75ab5f4c881701c133cb2b4f8d
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-05-02 11:40:18 -0500

    working on the exprience section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import LogoSection from "./components/LogoSection.jsx";
+import LogoShowcase from "./components/LogoShowcase.jsx";
+
 import NavBar from "./components/NavBar.jsx";
 import ExperienceSection from "./sections/ExperienceSection.jsx";
 import FeatureCards from "./sections/FeatureCards.jsx";
 import Hero from "./sections/hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import TechStack from "./sections/TechStack.jsx";
 
 const App = () => {
   return (
@@ -11,9 +13,10 @@ const App = () => {
       <NavBar />
       <Hero />
       <ShowcaseSection />
-      <LogoSection />
+      <LogoShowcase />
       <FeatureCards />
       <ExperienceSection />
+      <TechStack />
     </>
   );
 };
